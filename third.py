e6b2533f1c0f40ebb2ab400e184de045a32db4ad
Yash135-gif <gyash4098@gmail.com>
2026-04-26 21:26:12 +0530

    new code

--- third.py
+++ third.py
@@ -2,11 +2,11 @@ from first import add
 # print(a)
 # print(b)
 # print(c)
-n=int(input("enter the vlaue1"))
-m=int(input("enter the vlaue2"))
-print(add(n,m))
-a=50
-b=50
-res=add(a,b)
-print(res)
+# n=int(input("enter the vlaue1"))
+# m=int(input("enter the vlaue2"))
+# print(add(n,m))
+# a=50
+# b=50
+# res=add(a,b)
+# print(res)
 
