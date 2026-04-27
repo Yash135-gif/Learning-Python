25271d1fde3649dfdc41e9a27f51adf8b2843da2
Yash135-gif <gyash4098@gmail.com>
2026-04-27 23:21:43 +0530

    new code

--- OOP's.py
+++ OOP's.py
@@ -1,145 +1,145 @@
-# class Student:
-#     '''Student detail'''
-#     name="yash"
-#     age=20
+class Student:
+    '''Student detail'''
+    name="yash"
+    age=20
 
-# print(dir(Student))
+print(dir(Student))
 
-# print(Student__doc__)
+print(Student__doc__)
 
-# print(Student)
-# obj=Student
+print(Student)
+obj=Student
 
-# obj=Student()
-# print(obj)
+obj=Student()
+print(obj)
 
 
-# class Student:
-#     '''Student detail'''
+class Student:
+    '''Student detail'''
 
-#     def __init__(self):
-#         print("Constructor Called")
-#         print(id(self))
+    def __init__(self):
+        print("Constructor Called")
+        print(id(self))
 
-# obj=Student
-# obj=Student()
+obj=Student
+obj=Student()
 
-# print(id(obj))
+print(id(obj))
 
-# class Student:
-#     '''Student detail'''
+class Student:
+    '''Student detail'''
 
-#     def __init__(self,name,roll,marks):
-#         self.x=name
-#         self.y=roll
-#         self.z=marks
+    def __init__(self,name,roll,marks):
+        self.x=name
+        self.y=roll
+        self.z=marks
 
-# obj=Student("yash",27,80)
-# print(obj.x)
-# print(obj.y)
-# print(obj.z)
+obj=Student("yash",27,80)
+print(obj.x)
+print(obj.y)
+print(obj.z)
 
-# class Student:
-#     '''Student detail'''
+class Student:
+    '''Student detail'''
 
-#     def __init__(self,name,roll,marks):
-#            self.x=name
-#            self.y=roll
-#            self.z=marks
+    def __init__(self,name,roll,marks):
+           self.x=name
+           self.y=roll
+           self.z=marks
 
-#     def __init__ (self):
-#         print("construcor called")
+    def __init__ (self):
+        print("construcor called")
     
 
-# obj=Student()
-# obj.__init__()
+obj=Student()
+obj.__init__()
 
-    # I M P  ----------------------------------------------------------
+    I M P  ----------------------------------------------------------
 
-# class Student:
-#     def __init__(self,name,roll,marks):
-#         self.x=name
-#         self.y=roll
-#         self.z=marks
+class Student:
+    def __init__(self,name,roll,marks):
+        self.x=name
+        self.y=roll
+        self.z=marks
 
-#     def add_new(self,city):
-#         self.c=city
+    def add_new(self,city):
+        self.c=city
 
-#     def show(self):
-#         print(self.x,self.y,self.z,self.c,self.school_name)
+    def show(self):
+        print(self.x,self.y,self.z,self.c,self.school_name)
 
-# obj1=Student("yash",20,90)
-# obj1.add_new("bhopal")
-# obj1.school_name="STJP"
-# obj1.show()
+obj1=Student("yash",20,90)
+obj1.add_new("bhopal")
+obj1.school_name="STJP"
+obj1.show()
 
-# print(obj1.x,obj1.y,obj1.z,obj1.c,obj1.school_name)
+print(obj1.x,obj1.y,obj1.z,obj1.c,obj1.school_name)
 
-#   IMP 
+  IMP 
  
-# class Sports:
-#     def __init__(self,name1,name2,name3):
-#         self.a=name1
-#         self.b=name2
-#         self.c=name3
+class Sports:
+    def __init__(self,name1,name2,name3):
+        self.a=name1
+        self.b=name2
+        self.c=name3
 
-#     def add_player(self,name4,name5):
-#         self.d=name4
-#         self.e=name5
-#     def display(self):
-#         print(self.a,self.b,self.c,self.d,self.e,self.manager)
+    def add_player(self,name4,name5):
+        self.d=name4
+        self.e=name5
+    def display(self):
+        print(self.a,self.b,self.c,self.d,self.e,self.manager)
 
-# obj1=Sports("messi","abd","sky")
-# obj1.add_player("alien","musiala")
-# obj1.manager="Pep"
-# obj1.display()
+obj1=Sports("messi","abd","sky")
+obj1.add_player("alien","musiala")
+obj1.manager="Pep"
+obj1.display()
 
-    # IMP 
+    IMP 
 
-# class Student:
-#     school="SHSS"
-#     count=0
-#     def __init__(self,name,roll):
-#         self.x="name"
-#         self.y="roll"
-#         Student.s_code=123
-#         Student.count=Student.count+1
+class Student:
+    school="SHSS"
+    count=0
+    def __init__(self,name,roll):
+        self.x="name"
+        self.y="roll"
+        Student.s_code=123
+        Student.count=Student.count+1
 
-#     def new(self):
-#         Student.city="Bhopal"
+    def new(self):
+        Student.city="Bhopal"
 
-#     def show(self):
-#         print(Student.school,Student.s_code,Student.city,Student.subject)
+    def show(self):
+        print(Student.school,Student.s_code,Student.city,Student.subject)
 
     
-# obj=Student("yash",21)
-# Student.subject="pcm"
+obj=Student("yash",21)
+Student.subject="pcm"
 
-# print(Student.count)
-# print()
-# obj.show()
+print(Student.count)
+print()
+obj.show()
 
-# print(Student.school)
+print(Student.school)
 
-# class Party:
-#     person1="yash"
-#     def __init__(self,person2,person3):
-#         self.p2=person2
-#         self.p3=person3
-#         Party.person4="John Cena"
+class Party:
+    person1="yash"
+    def __init__(self,person2,person3):
+        self.p2=person2
+        self.p3=person3
+        Party.person4="John Cena"
 
-#     def add_person(self,person5):
-#         self.p5=person5
-#         print(self.p5)
+    def add_person(self,person5):
+        self.p5=person5
+        print(self.p5)
 
-#     def show(self):
-#         print(Party.person1,self.p2,self.p3,Party.person4,self.p5,Party.person6)
+    def show(self):
+        print(Party.person1,self.p2,self.p3,Party.person4,self.p5,Party.person6)
 
-# obj=Party("Messi","musiala")
-# obj.add_person("Rancho")
-# Party.person6="don't know"
-# obj.show()
-# print(Party.person6)
+obj=Party("Messi","musiala")
+obj.add_person("Rancho")
+Party.person6="don't know"
+obj.show()
+print(Party.person6)
 
 
 
